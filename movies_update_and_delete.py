f5f65f42c2b0946fb4ab62498bc283d56b2f869b
jamaregoat <134401605+jamaregoat@users.noreply.github.com>
2023-07-01 19:53:31 -0500

    Add files via upload

---
+++ movies_update_and_delete.py
@@ -0,0 +1,7 @@
+CREATE PROCEDURE `new_procedure` ()
+BEGINValue = int (input ("Type a number between 1 and 10: ")) 
+cursor.execute ("DELETE %s FROM test WHERE name='%s'" % Value) 
+Value2 = int (input ("Type a number between 11 and 20: ")) 
+cursor.execute ("UPDATE test SET id='abcd' WHERE name='%s'" %s Value2)
+
+END
